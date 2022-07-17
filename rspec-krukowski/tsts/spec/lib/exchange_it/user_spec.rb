5d651737fd9f1e57972651807b1e7b83a65adac9
paalso <40a.pvl@gmail.com>
2022-07-17 21:51:57 +0300

    Fix a bug in the first test

--- rspec-krukowski/tsts/spec/lib/exchange_it/user_spec.rb
+++ rspec-krukowski/tsts/spec/lib/exchange_it/user_spec.rb
@@ -3,6 +3,6 @@
 RSpec.describe ExchangeIt::User do
   it 'returns name' do
     user = ExchangeIt::User.new 'John', 'Doe'
-    user.name.expect(user.name).to eq('John')
+    expect(user.name).to eq('John')
   end
 end
