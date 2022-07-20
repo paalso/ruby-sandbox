a5560eb1669c39ca14134aa8b7c8d30a0f573dc1
pavel-heyjobs <pavel.soroka@heyjobs.de>
2022-07-20 16:45:06 +0300

    Continue

---
+++ rspec-krukowski/tsts/lib/exchange_it/account.rb
@@ -0,0 +1,12 @@
+module ExchangeIt
+
+  class Account
+    include ExchangeIt::Utils::Uid
+    attr_reader :uid, :balance
+
+    def initialize(user)
+      @uid = hash user.name, user.surname
+      @balance = 0
+    end
+  end
+end
--- rspec-krukowski/tsts/lib/exchange_it/user.rb
+++ rspec-krukowski/tsts/lib/exchange_it/user.rb
@@ -24,11 +24,22 @@
 
 module ExchangeIt
   class User
-    attr_reader :name, :surname
+    extend Forwardable
+
+    attr_reader :name, :surname, :account
+
+    def_delegators :account, :balance
 
     def initialize(name, surname)
       @name = name.to_s
       @surname = surname.to_s
+      create_account
+    end
+
+    private
+
+    def create_account
+      @account = Account.new self
     end
   end
 end
